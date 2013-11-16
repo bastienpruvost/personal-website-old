d2e6c94b5dcc1affb2344c5fee26381afd0f469c
bpruvost <bastien.pruvost@gmail.com>
2013-11-16 13:54:28 +0100

    Building the architecture for several pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+
+  def index
+  end
+
+  def cv
+  end
+
+  def projets
+  end
+
+  def blog
+  end
+
+  def contact
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Bastienpruvost::Application.routes.draw do
-  root 'welcome#index'
+  root 'pages#index'
+
+  get "/cv", :to => "pages#cv"
+  get "/projets", :to => "pages#projets"
+  get "/blog", :to => "pages#blog"
+  get "/contact", :to => "pages#contact"
+
 end
