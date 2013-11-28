59f64f803ca74f2508af1a8dec6e239632fff61b
bpruvost <bastien.pruvost@gmail.com>
2013-11-28 19:10:10 +0100

    Writing projects details: 7/16

--- config/routes.rb
+++ config/routes.rb
@@ -21,18 +21,18 @@ Bastienpruvost::Application.routes.draw do
   get '/projects/prp',                :to => "projects#prp"
   get '/projects/prt',                :to => "projects#prt"
 
-  get '/projects/alten_atexis',       :to => "projects#alten_atexis"
-  get '/projects/test_connaissances', :to => "projects#test_connaissances"
+  get '/projects/alten-atexis',       :to => "projects#alten-atexis"
+  get '/projects/test-connaissances', :to => "projects#test-connaissances"
   get '/projects/rao',                :to => "projects#rao"
 
-  get '/projects/bda_site',           :to => "projects#bda_site"
-  get '/projects/bda_projets',        :to => "projects#bda_projets"
+  get '/projects/bda-site',           :to => "projects#bda-site"
+  get '/projects/bda-projets',        :to => "projects#bda-projets"
 
-  get '/projects/tnd_wordpress',      :to => "projects#tnd_wordpress"
-  get '/projects/tnd_bpack',          :to => "projects#tnd_bpack"
-  get '/projects/tnd_francoisandco',  :to => "projects#tnd_francoisandco"
+  get '/projects/tnd-wordpress',      :to => "projects#tnd-wordpress"
+  get '/projects/tnd-bpack',          :to => "projects#tnd-bpack"
+  get '/projects/tnd-francoisandco',  :to => "projects#tnd-francoisandco"
   
-  get '/projects/actemium_dalkia',    :to => "projects#actemium_dalkia"
+  get '/projects/actemium-dalkia',    :to => "projects#actemium_dalkia"
   get '/projects/pinf',               :to => "projects#pinf"
 
 end
