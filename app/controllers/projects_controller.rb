405152d7eb74cece3c04916c9b6718e3a5eb533f
bpruvost <bastien.pruvost@gmail.com>
2013-12-01 15:44:05 +0100

    Error handling for project#show and contact#new

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,6 +5,10 @@ class ProjectsController < ApplicationController
   end
 
   def show
+    unless Project.where(common_name: params[:name]) != 1
+      flash[:error] = "Une erreur est survenue. Si ce probleme persiste, merci de me contacter."
+      redirect_to action: 'index' and return
+    end
     @project = Project.where(common_name: params[:name]).first
     @img_col_width = @project.image_column_width
   end
