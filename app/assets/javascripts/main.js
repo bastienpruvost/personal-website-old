4a77d418a076fda3eabde8979faf40aa74c9d570
bpruvost <bastien.pruvost@gmail.com>
2013-12-01 16:41:34 +0100

    Tweaking school page + adding projects links in CV

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,12 +6,15 @@ function startCV() {
 
 function toggleView(el) {
   desc = el.parent().find(".description")
-  if (desc.is(':visible')) {
+  info = el.parent().find(".info")
+  if (desc.is(':visible') || info.is(':visible')) {
     desc.hide();
+    info.hide();
     el.css("list-style-image","url('/assets/arrow_right.png')");
   }
   else {
     desc.show();
+    info.show();
     el.css("list-style-image","url('/assets/arrow_down.png')");
   }
 }
