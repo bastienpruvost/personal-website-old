0654cf41a7bca68fa6e736484edb7e9eb0e8738f
bpruvost <bastien.pruvost@gmail.com>
2013-12-13 15:56:11 +0100

    301 redirect to "/fr/URL" when no locale is specified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,20 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :set_locale, :set_variables
+  before_action :set_locale
 
   def set_locale
+    redirect_to "/fr"+request.fullpath and return unless params[:locale]
     I18n.locale = params[:locale]
-    redirect_to "/fr" unless params[:locale].in? ["en", "fr"]
+    @flag = (I18n.locale == :en) ? "fr" : "en"
+    @url_translated = request.fullpath.gsub(/^\/#{I18n.locale}/, "/"+@flag)
   end
 
   def default_url_options(options={})
     { locale: I18n.locale }
-  end
-
-  def set_variables
-    @flag = (I18n.locale == :en) ? "fr" : "en"
-    @url_translated = request.fullpath.gsub(/^\/#{I18n.locale}/, "/"+@flag)
   end
 
 end
